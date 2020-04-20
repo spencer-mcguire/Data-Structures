2991a81fedad006d9cb764c03774d8d383dcbc68
spencer-mcguire <smcguire1826@gmail.com>
2020-04-20 16:34:04 -0500

    queue

--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -2,10 +2,11 @@ import sys
 sys.path.append('../doubly_linked_list')
 from doubly_linked_list import DoublyLinkedList
 
+
 class Stack:
     def __init__(self):
         self.size = 0
-        # Why is our DLL a good choice to store our elements?
+        # Why is our DLL a good choice to store our elements? stack opperates on FIFO just good practice
         # self.storage = ?
 
     def push(self, value):
